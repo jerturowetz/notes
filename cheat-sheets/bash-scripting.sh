f6a90284e671b747619f66f75848433e406dfbbf
Jeremy Turowetz <j.turowetz@gmail.com>
2018-08-11 18:54:46 -0400

    add example function and docblock

--- cheat-sheets/bash-scripting.sh
+++ cheat-sheets/bash-scripting.sh
@@ -14,3 +14,24 @@ for D in *; do [ -d "${D}" ] && my_command; done
 
 # Or an even more concise version (thanks @enzotib). Note that in this version each value of D will have a trailing slash
 for D in */; do my_command; done
+
+# --------------------------------------------------------- #
+# prepend_to_file ()                                        #
+# Prepends string to given file                             #
+# Parameter: $file_name                                     #
+# Parameter: $prepend_string                                #
+# Returns: 0 on success, $E_BADFILE if file not present     #
+# --------------------------------------------------------- #
+prepend_to_file () {
+
+	local file_name = "${1}"
+	local prepend_string = "${2}"
+
+	if [ ! -f "${file_name}" ]; then
+		return $E_BADFILE;
+    fi
+
+	mv "${file_name}" "${prepend_string}--${file_name}"
+	return 0;
+
+}
