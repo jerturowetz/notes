a1a47060820b300e18c72e077965478fe71f4805
Jeremy Turowetz <j.turowetz@gmail.com>
2019-05-17 16:01:50 -0400

    update cheat sheets

--- cheat-sheets/wp-cli.sh
+++
@@ -1,3 +0,0 @@
-# Disable all comments/pings in WordPress with WP-CLI
-wp post list --format=ids | xargs wp post update --comment_status=closed
-wp post list --format=ids | xargs wp post update --ping_status=closed
---
+++ cheat-sheets/wp-engine.md
@@ -0,0 +1,5 @@
+# Cheat sheet : WP Engine
+
+## WPE log format
+
+    '$remote_addr $http_host $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$wpe_user_agent"';
