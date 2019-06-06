db96103d54ea9192b490ec1e39da9493fd0f8587
Jeremy Turowetz <j.turowetz@gmail.com>
2019-06-06 18:14:24 -0400

    add example default output

--- cheat-sheets/bash-scripting.sh
+++ cheat-sheets/bash-scripting.sh
@@ -3,6 +3,27 @@
 # The shell style ghuide by google is great
 # https://google.github.io/styleguide/shell.xml#File_Header
 
+
+## Example usage function
+usage()
+{
+cat << EOF
+usage: $0 SOME_PASSED_VARIABLE
+
+Explain what the function does and what parameters are expected and what they do
+
+EOF
+}
+
+## Check for a passed variable and call usage if missing
+SOME_PASSED_VARIABLE="${1}"
+
+if [[ -z $SOME_PASSED_VARIABLE ]]
+then
+  usage
+  return
+fi
+
 # Loops trough folders and then you can do whatever
 for D in *; do
     if [ -d "${D}" ]; then
